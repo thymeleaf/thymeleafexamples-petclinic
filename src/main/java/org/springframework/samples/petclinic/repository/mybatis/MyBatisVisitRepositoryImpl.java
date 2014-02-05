cf0830d1cf1198c0e301e0c8efaba18309a72b0c
Jose Juan Montiel <josejuan.montiel@tecnilogica.com>
2014-02-05 16:50:05 +0100

    Implementacion del repositorio que accede a los mapper de mybatis.

---
+++ src/main/java/org/springframework/samples/petclinic/repository/mybatis/MyBatisVisitRepositoryImpl.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.repository.mybatis;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Visit;
+import org.springframework.samples.petclinic.repository.VisitRepository;
+import org.springframework.samples.petclinic.repository.mybatis.mapper.VisitMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * JPA implementation of the ClinicService interface using EntityManager.
+ * <p/>
+ * <p>The mappings are defined in "orm.xml" located in the META-INF directory.
+ *
+ * @author Mike Keith
+ * @author Rod Johnson
+ * @author Sam Brannen
+ * @author Michael Isvy
+ * @since 22.4.2006
+ */
+@Repository
+public class MyBatisVisitRepositoryImpl implements VisitRepository {
+
+	@Autowired
+    private VisitMapper visitMapper;
+
+
+    @Override
+    public void save(Visit visit) {
+    	visitMapper.save(visit);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Visit> findByPetId(Integer petId) {
+    	return visitMapper.findByPetId(petId);
+    }
+
+}
