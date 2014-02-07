434642a7d1dab94967444540966b858dbc5b9264
Jose Juan Montiel <josejuan.montiel@tecnilogica.com>
2014-02-07 12:56:58 +0100

    Creamos una tabla todo... para implementar el ejemplo "todo" en esta version... con mybatis...

--- src/main/resources/db/hsqldb/populateDB.sql
+++ src/main/resources/db/hsqldb/populateDB.sql
@@ -51,3 +51,7 @@ INSERT INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
 INSERT INTO visits VALUES (3, 8, '2013-01-03', 'neutered');
 INSERT INTO visits VALUES (4, 7, '2013-01-04', 'spayed');
+
+INSERT INTO todo VALUES (1, 'Tarea1', 2, 1);
+INSERT INTO todo VALUES (2, 'Tarea2', 3, 0);
+INSERT INTO todo VALUES (3, 'Tarea3', 1, 0);
